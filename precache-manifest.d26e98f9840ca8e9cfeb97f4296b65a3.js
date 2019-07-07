ac8478e211f166bec35210d5cfed591d50f972f0
LeoVS09 <leovs010@gmail.com>
2019-07-07 22:27:08 +0300

    Updates

---
+++ precache-manifest.d26e98f9840ca8e9cfeb97f4296b65a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8613c12718084eccb6ae566c6ed41d25",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b300b8c6b22ba27a942c",
+    "url": "/Tic-tac-toe/static/css/3.7e8339da.chunk.css"
+  },
+  {
+    "revision": "72695a686bf88f6dd875",
+    "url": "/Tic-tac-toe/static/css/main.ef8592a4.chunk.css"
+  },
+  {
+    "revision": "7089e2ed66fe99d5aae1",
+    "url": "/Tic-tac-toe/static/js/2.757ea32a.chunk.js"
+  },
+  {
+    "revision": "b300b8c6b22ba27a942c",
+    "url": "/Tic-tac-toe/static/js/3.01721c9e.chunk.js"
+  },
+  {
+    "revision": "7b53adae8cc5466f67f6",
+    "url": "/Tic-tac-toe/static/js/4.a2eb6da9.chunk.js"
+  },
+  {
+    "revision": "72695a686bf88f6dd875",
+    "url": "/Tic-tac-toe/static/js/main.4fc6c033.chunk.js"
+  },
+  {
+    "revision": "dbc1591450b93c796f5e",
+    "url": "/Tic-tac-toe/static/js/runtime~main.80edf8d6.js"
+  }
+]);
